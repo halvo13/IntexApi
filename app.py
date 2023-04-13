24a8c9e14b527370c24f15f72e6456bc1bfb85b1
jhalver6 <jhalver6@byu.edu>
2023-04-13 16:08:55 -0600

    new pickle

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ async def read_root():
 @app.post('/predict')
 def predict(data:dict):
   # Load model from .pkl file
-  with open('./model.pkl','rb') as file:
+  with open('./dc_model.pkl','rb') as file:
     model = pickle.load(file)
     # Convert input data to DataFrame
     df = pd.DataFrame(data, index=[0])
